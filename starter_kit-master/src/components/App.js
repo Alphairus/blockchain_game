1396464f0caf0c548b4eafe79454c32e937ad71d
Alphairus <cmcauliffe006@gmail.com>
2021-04-13 16:29:50 -0400

    file prep

--- starter_kit-master/src/components/App.js
+++ starter_kit-master/src/components/App.js
@@ -1,8 +1,19 @@
 import React, { Component } from 'react';
-import logo from '../logo.png';
+import Web3 from 'web3'
 import './App.css';
+import MemoryToken from '../abis/MemoryToken.json'
+import brain from '../brain.png'
 
 class App extends Component {
+
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      account: '0x0'
+    }
+  }
+
   render() {
     return (
       <div>
@@ -13,33 +24,41 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Dapp University
+          <img src={brain} width="30" height="30" className="d-inline-block align-top" alt="" />
+          &nbsp; Memory Tokens
           </a>
+          <ul className="navbar-nav px-3">
+            <li className="nav-item text-nowrap d-none d-sm-none d-sm-block">
+              <small className="text-muted"><span id="account">{this.state.account}</span></small>
+            </li>
+          </ul>
         </nav>
         <div className="container-fluid mt-5">
           <div className="row">
             <main role="main" className="col-lg-12 d-flex text-center">
               <div className="content mr-auto ml-auto">
-                <a
-                  href="http://www.dappuniversity.com/bootcamp"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <img src={logo} className="App-logo" alt="logo" />
-                </a>
-                <h1>Dapp University Starter Kit</h1>
-                <p>
-                  Edit <code>src/components/App.js</code> and save to reload.
-                </p>
-                <a
-                  className="App-link"
-                  href="http://www.dappuniversity.com/bootcamp"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  LEARN BLOCKCHAIN <u><b>NOW! </b></u>
-                </a>
+                <h1 className="d-4">Edit this file in App.js!</h1>
+
+                <div className="grid mb-4" >
+
+                  {/* Code goes here... */}
+
+                </div>
+
+                <div>
+
+                  {/* Code goes here... */}
+
+                  <div className="grid mb-4" >
+
+                    {/* Code goes here... */}
+
+                  </div>
+
+                </div>
+
               </div>
+
             </main>
           </div>
         </div>
